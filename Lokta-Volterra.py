27ba01092a6a19a5f92b4fbaa04b1a70efda0413
beardotheweirdo1 <45885605+jake-beardo@users.noreply.github.com>
2021-03-07 21:19:07 +0000

    version 3.0 (working)
    can handle any order of ODE and code is more concise

--- Lokta-Volterra.py
+++ Lokta-Volterra.py
@@ -1,23 +1,17 @@
 import numpy as np
 from solver_functions import *
 
-def dx_dt(vars,a=1,b=0.3,d=0.1):
-    dx_dt = vars[0]*(1-vars[0]) - (a*vars[0]*vars[1])/(d+vars[0])
-    return dx_dt
-
-def dy_dt(vars,a=1,b=0.3,d=0.1):
-    dy_dt = b*vars[1]*(1 - (vars[1]/vars[0]))
-    return dy_dt
-
+def lokta(vars,t,a,b,d):
+    return np.array([vars[0]*(1-vars[0]) - (a*vars[0]*vars[1])/(d+vars[0]), b*vars[1]*(1 - (vars[1]/vars[0]))])
 
 a = 1
 d = 0.1
 b = np.linspace(0.1,0.5,100)
 b = 0.1
 t0 = 0
-tt = 400
-n=tt-1
+tt = 200
+n = 500
 step_sizes =  np.logspace(-3,(t0 + int((tt-t0)/n)),8)
-ODE = dx_dt, dy_dt
+ODE = lokta
 # main(t0,tt,x0,y0,ODE, deltat_max, step_sizes)
-main(t0,tt,0.1,0.1,ODE, 0.5, step_sizes)
+main(t0,tt,1,0.5,ODE,n, 0.5, step_sizes,a=1,b=0.1,d=0.1)
--- solver_functions.py
+++ solver_functions.py
@@ -16,88 +16,70 @@ def analytical_sol(t0,t1, step_sizes, sol_x):
         x_a[i] = sol_x(t_a[i])
     return t_a,x_a
 
-def euler_step(vars, t_pre, ODE, step_size):
-    x_new = vars[0] + step_size * ODE(vars,t_pre)
-    y_new = vars[1] + step_size * ODE(vars,t_pre)
-    return [x_new,y_new]
+def euler_step(vars, t_pre, ODE, step_size,**kwargs):
+    vars = vars + step_size * ODE(vars,t_pre,**kwargs)
+    return vars
 
 
 # N can be outside function make function so its just one step
-def rk4(vars, t_pre, ODE, step_size):
-    k1 = step_size*ODE(vars,t_pre)
-    k2 = step_size*ODE(vars+(k1/2),t_pre+(step_size/2))
-    k3 = step_size*ODE(vars+(k2/2),t_pre+(step_size/2))
-    k4 = step_size*ODE(vars+(k3),t_pre+(step_size))
+def rk4(vars, t_pre, ODE, step_size,**kwargs):
+    k1 = step_size*ODE(vars,t_pre,**kwargs)
+    k2 = step_size*ODE(vars+(k1/2),t_pre+(step_size/2),**kwargs)
+    k3 = step_size*ODE(vars+(k2/2),t_pre+(step_size/2),**kwargs)
+    k4 = step_size*ODE(vars+(k3),t_pre+(step_size),**kwargs)
     k = (1/6)*(k1 + 2*k2 + 2*k3 + k4)
-    y_new = vars[1] + k
-    x_new = vars[0] + k
-    return [x_new,y_new]
+    vars = vars + k
+    return vars
 
 
-def do_step(vars,t0,step_size,ODE,n,extra_step,rk_e):
+def do_step(vars,t0,step_size,ODE,n,extra_step,rk_e,**kwargs):
     # returns x_arrays which is the xs between each step
     t = t0
     if rk_e == "--euler":
         for i in range(n):
-            y_new = euler_step(vars, t, ODE[1], step_size)[1]
-            x_new = euler_step(vars, t, ODE[0], step_size)[0]
-            vars[1] = y_new
-            vars[0] = x_new
+            vars = euler_step(vars, t, ODE, step_size,**kwargs)
+
             t += step_size
-        y_extra = euler_step(vars, t, ODE[1], extra_step)[1]
-        x_extra = euler_step(vars, t, ODE[0], extra_step)[0]
+        vars = euler_step(vars, t, ODE, extra_step,**kwargs)
     elif rk_e == "--runge":
         for i in range(n):
+
             # rk4(x_pre, t_pre, ODE, step_size)
-            y = rk4(vars, t, ODE[1], step_size)[1]
-            x = rk4(vars, t, ODE[0], step_size)[0]
-            vars[1] = y
-            vars[0] = x
+            vars = rk4(vars, t, ODE, step_size,**kwargs)
             t += step_size
-        y_extra = rk4(vars, t, ODE[1], extra_step)[1]
-        x_extra = rk4(vars, t, ODE[0], extra_step)[0]
-    t += extra_step
-    vars[1] = y_extra
-    vars[0] = x_extra
+        vars = rk4(vars, t, ODE, extra_step,**kwargs)
     return vars
 
 
-def solve_to(vars,t0,ODE, t2,step_size,rk_e):
+def solve_to(vars,t0,ODE, t2,step_size,rk_e,**kwargs):
     gap = t2-t0
     if step_size % gap == 0:
         n = gap/step_size
         extra_step = 0
-        vars = do_step(vars,t0,step_size,ODE,n,extra_step,rk_e)
+        vars = do_step(vars,t0,step_size,ODE,n,extra_step,rk_e,**kwargs)
     else:
         n = (gap/step_size).astype(int)
         extra_step = gap - n*step_size
-        vars = do_step(vars,t0,step_size,ODE,n,extra_step,rk_e)
-
-    return vars[0],vars[1]
+        vars = do_step(vars,t0,step_size,ODE,n,extra_step,rk_e,**kwargs)
+    return vars
 
 # n is the number of x's wanted between x0 and target solution
-def solve_ode(inits,t0,tt, n, ODE,step_size, rk_e):
-    t_vals = np.zeros(n+3)
-    x_sols = np.zeros(n+2)
-    y_sols = np.zeros(n+2)
-    t_vals[0] = t0
-    x_sols[0] = inits[0]
-    y_sols[0] = inits[1]
-    print(tt,t0,n)
-    steps = int((tt-t0)/n)
+def solve_ode(inits,t0,tt, n, ODE,step_size, rk_e, **kwargs):
+    x_sols = [inits[0]]
+    y_sols = [inits[1]]
+    t_vals = [t0]
+    vars = inits
+    steps = (tt-t0)/n
+    print(tt,t0,n,steps)
     t2 = t0 + steps
-    t_vals[1] = t2
     for i in range(n+1):
-        vars = [x_sols[i],y_sols[i]]
-        x_sol,y_sol = solve_to(vars,t_vals[i],ODE, t_vals[i+1],step_size,rk_e)
-        x0 = x_sol
-        y0 = y_sol
+        vars = solve_to(vars, t0, ODE, t2, step_size, rk_e, **kwargs)
+        x_sols.append(vars[0])
+        y_sols.append(vars[1])
+        t0 = t2
         t2 += steps
-        t_vals[i+2] = t2
-        x_sols[i+1] = x0
-        y_sols[i+1] = y0
-    t_vals = t_vals[:-1]
-    return t_vals, x_sols,y_sols
+        t_vals.append(t0)
+    return t_vals,x_sols,y_sols
 
 def error_finder(x_sols_array,t_vals_array,sol_x):
     first_counter = 0
@@ -111,10 +93,8 @@ def error_finder(x_sols_array,t_vals_array,sol_x):
 
 #sol = odeint(ODE, x0, delta_t, args=(x,t)
 
-def main(t0,tt,x0,y0,ODE, deltat_max, step_sizes):
-    inits = [x0,y0]
-    n = tt-1
-    print(n)
+def main(t0,tt,x0,y0,ODE,n, deltat_max, step_sizes,**kwargs):
+    inits = np.array([x0,y0])
     idxs_array = []
     t_vals_array = []
     x_sols_array = []
@@ -128,16 +108,13 @@ def main(t0,tt,x0,y0,ODE, deltat_max, step_sizes):
             idxs_array.append(idx)
         else:
             # (x0,t0,tt, n, ODE,deltat_max, rk_e) use sys to run
-
-            t_vals_runge, x_sols_runge, y_sols_runge = solve_ode(inits,t0,tt, n, ODE,step_sizes[j],"--runge")
-            t_vals, x_sols,y_sols = solve_ode(inits,t0,tt, n, ODE,step_sizes[j],"--euler")
+            t_vals_runge, x_sols_runge,y_sols_runge = solve_ode(inits,t0,tt, n, ODE,step_sizes[j],"--runge",**kwargs)
+            t_vals, x_sols,y_sols = solve_ode(inits,t0,tt, n, ODE,step_sizes[j],"--euler",**kwargs)
             t_vals_array.append(t_vals)
             x_sols_array.append(x_sols)
             x_sols_array_runge.append(x_sols_runge)
             y_sols_array.append(y_sols)
             y_sols_array_runge.append(y_sols_runge)
-
-
     step_sizes = np.delete(step_sizes, idxs_array)
     #step_sizes =  np.linspace(0,1,101) # stepsize
 
@@ -164,7 +141,6 @@ def main(t0,tt,x0,y0,ODE, deltat_max, step_sizes):
     plt.title("euler method approximation")
     errs = error_finder(x_sols_array,t_vals_array,sol_x)
     errs_runge = error_finder(x_sols_array_runge,t_vals_array,sol_x)
-    print(errs_runge)
     plt.loglog(step_sizes, errs, label="euler")
     plt.loglog(step_sizes, errs_runge, label="runge")
     plt.ylabel("Error")
